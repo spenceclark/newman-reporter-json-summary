6e9a3c9d8217c7037a86fc655b3624184429f233
Spencer <spencer@portemis.com>
2019-08-04 16:37:03 +0100

    Fixing issue #1
    --reporter-json-summary-export does not work together with standard JSON reporter

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ module.exports = function(newman, options) {
         newman.exports.push({
             name: 'newman-reporter-json-summary',
             default: 'summary.json',
-            path:  options.jsonSummaryExport,
+            path:  options.summaryJsonExport,
             content: JSON.stringify(createSummary(data.summary))
         });
     });
